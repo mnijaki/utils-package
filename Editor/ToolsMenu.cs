9ae082c45d3b1c12c6ecf0aac176b36ebbe35ba4
mnijaki <mnijaki@gmail.com>
2022-07-08 15:53:13 +0200

    * Added Utilities package

---
+++ Editor/ToolsMenu.cs
@@ -0,0 +1,29 @@
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace MarekNijaki.Utils
+{
+    public static class Tools
+    {
+        [MenuItem("Tools/Setup/Create default folders")]
+        public static void CreateDefaultFolders()
+        {
+            CreateFolders("_DefaultFolders", "Art", "Audio", "Data", "Prefabs", "Scripts");
+            CreateFolders("_DefaultFolders/Audio", "AudioSFX", "Dialogues", "Music");
+            CreateFolders("_DefaultFolders/Art", "Materials", "Models", "Sprites", "Textures");
+            // Refresh is needed so new folders appear in editor.
+            AssetDatabase.Refresh();
+        }
+
+        private static void CreateFolders(string rootFolder, params string[] foldersToCreate)
+        {
+            // Application.dataPath returns path to "Assets" directory.
+            string fullPath = Path.Combine(Application.dataPath, rootFolder);
+            foreach(string folderName in foldersToCreate)
+            {
+                Directory.CreateDirectory(Path.Combine(fullPath, folderName));
+            }
+        }
+    }
+}
