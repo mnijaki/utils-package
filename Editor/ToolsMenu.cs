14f14e5b0e943efc036b55cf7c45b6b7ec77402e
mnijaki <mnijaki@gmail.com>
2022-07-08 23:36:54 +0200

    * Added new funcionality - now folder will be created inside currently selected folder

--- Editor/ToolsMenu.cs
+++ Editor/ToolsMenu.cs
@@ -6,23 +6,45 @@ namespace MarekNijaki.Utils.Editor
 {
     public static class Tools
     {
+        private static string directoryPathToCurrentlySelectedFile;
+        
         [MenuItem("Tools/Setup/Create default folders")]
         public static void CreateDefaultFolders()
         {
-            CreateFolders("_DefaultFolders", "Art", "Audio", "Data", "Prefabs", "Scripts");
-            CreateFolders("_DefaultFolders/Audio", "AudioSFX", "Dialogues", "Music");
-            CreateFolders("_DefaultFolders/Art", "Materials", "Models", "Sprites", "Textures");
+            GetDirectoryPathToCurrentlySelectedFile();
+            CreateFolders(string.Empty, "Art", "Audio", "Data", "Prefabs", "Scripts");
+            CreateFolders("Scripts", "Runtime", "Editor");
+            CreateFolders("Audio", "AudioSFX", "Dialogues", "Music");
+            CreateFolders("Art", "Materials", "Models", "Sprites", "Textures");
             // Refresh is needed so new folders appear in editor.
             AssetDatabase.Refresh();
         }
 
         private static void CreateFolders(string rootFolder, params string[] foldersToCreate)
         {
-            // Application.dataPath returns path to "Assets" directory.
-            string fullPath = Path.Combine(Application.dataPath, rootFolder);
+            string fullPath = Path.Combine(directoryPathToCurrentlySelectedFile, rootFolder);
             foreach(string folderName in foldersToCreate)
             {
                 Directory.CreateDirectory(Path.Combine(fullPath, folderName));
+            }
+        }
+
+        private static void GetDirectoryPathToCurrentlySelectedFile()
+        {
+            // "Application.dataPath" returns path to "Assets" directory.
+            directoryPathToCurrentlySelectedFile = Application.dataPath;    
+            
+            Object[] selectedObjects = Selection.GetFiltered(typeof(Object), SelectionMode.Assets);
+            foreach(Object selectedObject in selectedObjects)
+            {
+                string path = AssetDatabase.GetAssetPath(selectedObject);
+                if(string.IsNullOrEmpty(path))
+                {
+                    continue;
+                }
+                
+                directoryPathToCurrentlySelectedFile = File.Exists(path) ? Path.GetDirectoryName(path) : path;
+                break;
             }
         }
     }
