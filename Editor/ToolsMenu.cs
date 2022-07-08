e2be8a0e4e19f4094c28e377badb0205090d0b1f
mnijaki <mnijaki@gmail.com>
2022-07-08 16:07:56 +0200

    * Added "Utils" class to runtime folder * Added assembly definitions to runtime and editor folders * Fixed namespaces

--- Editor/ToolsMenu.cs
+++ Editor/ToolsMenu.cs
@@ -2,7 +2,7 @@ using System.IO;
 using UnityEditor;
 using UnityEngine;
 
-namespace MarekNijaki.Utils
+namespace MarekNijaki.Utils.Editor
 {
     public static class Tools
     {
---
+++ Runtime/Utils.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace MarekNijaki.Utils
+{
+	public class Utils : MonoBehaviour
+	{
+		public static void CustomDebugLog()
+		{
+			Debug.Log("It's alive !!!");
+		}
+	}
+}
